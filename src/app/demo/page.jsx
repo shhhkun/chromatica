e5c6728443fe4d865f7f4a7e9c39c6d35cae98d5
Serjo <serjobarron@gmail.com>
2025-09-26 15:31:14 -0700

    converted jfif/png to webp

--- src/app/demo/page.jsx
+++ src/app/demo/page.jsx
@@ -41,144 +41,144 @@ const demoPalettes = [
 const demoArtists = [
   {
     name: "Epik High",
-    artistImageUrl: "/epikhigh.jfif",
+    artistImageUrl: "/epikhigh.webp",
   },
   {
     name: "keshi",
-    artistImageUrl: "/keshi.jfif",
+    artistImageUrl: "/keshi.webp",
   },
   {
     name: "Sarah Kang",
-    artistImageUrl: "/sarahkang.jfif",
+    artistImageUrl: "/sarahkang.webp",
   },
   {
     name: "Joe Hisaishi",
-    artistImageUrl: "/joe.jfif",
+    artistImageUrl: "/joe.webp",
   },
   {
     name: "Luke Chiang",
-    artistImageUrl: "/lukechiang.jfif",
+    artistImageUrl: "/lukechiang.webp",
   },
 ];
 
 const demoTracks = [
   {
     name: "Bodies", // 1
-    albumArtUrl: "/bodies.jfif",
+    albumArtUrl: "/bodies.webp",
     artist: "keshi",
     bgColor: { rgb: hexToRgb("#8c8474") },
   },
   {
     name: "ocean blues", // 2
-    albumArtUrl: "/oceanblues.jfif",
+    albumArtUrl: "/oceanblues.webp",
     artist: "demxntia",
     bgColor: { rgb: hexToRgb("#6e8ca0") },
   },
   {
     name: "Used To Me", // 3
-    albumArtUrl: "/usedtome.jfif",
+    albumArtUrl: "/usedtome.webp",
     artist: "Luke Chiang",
     bgColor: { rgb: hexToRgb("#655d9c") },
   },
   {
     name: "Guilty", // 4
-    albumArtUrl: "/guilty.jfif",
+    albumArtUrl: "/guilty.webp",
     artist: "Dynamicduo",
     bgColor: { rgb: hexToRgb("#a07150") },
   },
   {
     name: "Never Tell", // 5
-    albumArtUrl: "/nevertell.jfif",
+    albumArtUrl: "/nevertell.webp",
     artist: "Luke Chiang",
     bgColor: { rgb: hexToRgb("#6184a4") },
   },
   {
     name: "BRB", // 6
-    albumArtUrl: "/brb.jfif",
+    albumArtUrl: "/brb.webp",
     artist: "Epik High",
     bgColor: { rgb: hexToRgb("#848474") },
   },
   {
     name: "From the Rain (Feat. Ahn Ye Eun)", // 7
-    albumArtUrl: "/fromtherain.jfif",
+    albumArtUrl: "/fromtherain.webp",
     artist: "Heize, Ahn Ye Eun",
     bgColor: { rgb: hexToRgb("#975355") },
   },
   {
     name: "One Summer Day", // 8
-    albumArtUrl: "/onesummerday.jfif",
+    albumArtUrl: "/onesummerday.webp",
     artist: "Joe Hisaishi",
     bgColor: { rgb: hexToRgb("#948067") },
   },
   {
     name: "in your arms", // 9
-    albumArtUrl: "/inyourarms.jfif",
+    albumArtUrl: "/inyourarms.webp",
     artist: "Saib",
     bgColor: { rgb: hexToRgb("#5e9570") },
   },
   {
     name: "HOME IS FAR AWAY", // 10
-    albumArtUrl: "/homeisfaraway.jfif",
+    albumArtUrl: "/homeisfaraway.webp",
     artist: "Epik High, OHHYUK",
     bgColor: { rgb: hexToRgb("#8c8c8c") },
   },
   {
     name: "11월1일 (feat. 김재석)", // 11
-    albumArtUrl: "/nov1st.jfif",
+    albumArtUrl: "/nov1st.webp",
     artist: "Epik High, 김재석",
     bgColor: { rgb: hexToRgb("#9c7464") },
   },
   {
     name: "Some", // 12
-    albumArtUrl: "/some.jfif",
+    albumArtUrl: "/some.webp",
     artist: "BOL4",
     bgColor: { rgb: hexToRgb("#a48c64") },
   },
   {
     name: "ghosts", // 13
-    albumArtUrl: "/ghosts.jfif",
+    albumArtUrl: "/ghosts.webp",
     artist: "Heiakim, Sachi Gomez",
     bgColor: { rgb: hexToRgb("#8f7268") },
   },
   {
     name: "MISSING U", // 14
-    albumArtUrl: "/missingu.jfif",
+    albumArtUrl: "/missingu.webp",
     artist: "LeeHi",
     bgColor: { rgb: hexToRgb("#a47c54") },
   },
   {
     name: "bittersweet", // 15
-    albumArtUrl: "/bittersweet.jfif",
+    albumArtUrl: "/bittersweet.webp",
     artist: "Sarah Kang, Luke Chiang",
     bgColor: { rgb: hexToRgb("#8c7455") },
   },
   {
     name: "light", // 16
-    albumArtUrl: "/light.jfif",
+    albumArtUrl: "/light.webp",
     artist: "wave to earth",
     bgColor: { rgb: hexToRgb("#7987aa") },
   },
   {
     name: "if it's not you", // 17
-    albumArtUrl: "/ifitsnotyou.jfif",
+    albumArtUrl: "/ifitsnotyou.webp",
     artist: "PRYVT",
     bgColor: { rgb: hexToRgb("#747c84") },
   },
   {
     name: "Loving You From A Distance", // 18
-    albumArtUrl: "/lovingyoufromadistance.jfif",
+    albumArtUrl: "/lovingyoufromadistance.webp",
     artist: "jomm, readyaimfire27",
     bgColor: { rgb: hexToRgb("#784d86") },
   },
   {
     name: "Reminiscence", // 19
-    albumArtUrl: "/reminiscence.jfif",
+    albumArtUrl: "/reminiscence.webp",
     artist: "Vanilla Mood",
     bgColor: { rgb: hexToRgb("#a99e57") },
   },
   {
     name: "Rain - Long Ver.", // 20
-    albumArtUrl: "/rain.jfif",
+    albumArtUrl: "/rain.webp",
     artist: "Motohiro Hata",
     bgColor: { rgb: hexToRgb("#539f5a") },
   },
@@ -259,7 +259,7 @@ const DemoPage = () => {
                        my-4 mr-4 sm:my-8 sm:mr-8 md:my-10 md:mr-10 lg:my-12 lg:mr-12 flex-shrink-0"
           >
             <img
-              src="/bear.png"
+              src="/bear.webp"
               alt="Spotify Profile"
               className="object-cover transition-transform duration-200 ease-in-out hover:scale-110"
             />
